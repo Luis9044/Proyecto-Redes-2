5f3574d1ead6847501a46bd2c06793c9f11b4aef
jorge1b3 <locodeira1b3@gmail.com>
2023-07-18 20:30:39 -0500

    Arreglando

--- src/main/java/com/luis/parcial/ParcialApplication.java
+++ src/main/java/com/luis/parcial/ParcialApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class ParcialApplication {
 
 	public static void main(String[] args) {
